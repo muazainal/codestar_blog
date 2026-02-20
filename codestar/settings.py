217d7deec45b400e427269a0d735759a6c2cd668
muazainal <muazainal@live.com>
2026-02-20 20:58:59 +0000

    Use Heroku config vars instead of env.py

--- codestar/settings.py
+++ codestar/settings.py
@@ -14,11 +14,7 @@ import os
 from pathlib import Path
 import dj_database_url
 
-# Optional local env.py (for local development only)
-try:
-    from env import *
-except ImportError:
-    pass
+
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -29,10 +25,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# Use environment variable if available, else fallback for local dev
+SECRET_KEY = os.environ.get("SECRET_KEY", "django-insecure-local-dev-key")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+
 
 ALLOWED_HOSTS = ['.herokuapp.com', 'localhost', '127.0.0.1']
 
