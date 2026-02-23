02dd24c00a678ba4d49c3fc5a03f57fa6c0497a6
muazainal <muazainal@live.com>
2026-02-23 13:53:25 +0000

    Configure WhiteNoise for static files

--- codestar/settings.py
+++ codestar/settings.py
@@ -56,6 +56,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware", # ... other middleware
 ]
 
 ROOT_URLCONF = 'codestar.urls'
