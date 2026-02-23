7c1896635e058f72bc90f89ce2bedfacc494a108
muazainal <muazainal@live.com>
2026-02-23 13:27:23 +0000

    Update CSRF_TRUSTED_ORIGINS and DEBUG

--- codestar/settings.py
+++ codestar/settings.py
@@ -148,3 +148,8 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    "Http://*.codeinstitute-ide.net/",
+    "Http://*.herokuapp.com"
+]
